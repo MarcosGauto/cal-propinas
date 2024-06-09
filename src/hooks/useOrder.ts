63859286dbbd9a72046d0b08c0b7473b09785215
Marcos <mgauto@outlook.es>
2024-06-09 19:34:12 -0300

    mostrando en pantalla

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -6,8 +6,23 @@ export default function useOrder(){
     const[order, setOrder] = useState<OrderItem[]>([]) // aca se usa el generic <> para decirle que es tipo order item
 
   const addItem = (item : MenuItem) => {
-    
+    const itemExist = order.find(orderItem => orderItem.id === item.id)  //hacemos un order.find para encontrar un elemento en base a una condicion
+    if(itemExist){
+        const updateOrder = order.map( orderItem => orderItem.id === item.id ?
+           {...orderItem, quantity: orderItem.quantity + 1 } : 
+           orderItem
+
+        )
+        setOrder(updateOrder)
+
+    }else{
+
+    const newItem : OrderItem = {...item, quantity: 1} //generamos newitem con copia de item y agregamos cantidad
+    setOrder([...order, newItem])
+    }
   }
+
+  
 
     return{
         addItem
